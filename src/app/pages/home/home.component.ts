c100bf7f24b5002be34fc26d224ab6bd640aa506
Gabriel <ga.lima2205@gmail.com>
2024-12-26 19:09:42 -0300

    update styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
 import { HeaderComponent } from "../../components/header/header.component";
-import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, HeaderComponent, BannerComponent],
+  imports: [CardComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
